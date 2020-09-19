bc3631e79497ed9080303dac6aa77273868076ab
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-19 11:06:38 -0600

    Update test variables

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -7,16 +7,16 @@ RSpec.describe 'Weather Service' do
     keys3 = [:dt, :temp, :feels_like, :pressure, :humidity, :dew_point, :clouds, :visibility, :wind_speed, :wind_deg, :weather, :pop]
     keys4 = [:dt, :sunrise, :sunset, :temp, :feels_like, :pressure, :humidity, :dew_point, :wind_speed, :wind_deg, :weather, :uvi]
 
-    weather = WeatherService.new.get_forecast(38.892062, -77.019912)
+    weather_data = WeatherService.new.get_forecast(38.892062, -77.019912)
     keys2.each do |key|
-      expect(weather[:current]).to have_key(key)
+      expect(weather_data[:current]).to have_key(key)
     end
-    expect(weather).to_not have_key(:minutely)
+    expect(weather_data).to_not have_key(:minutely)
     keys3.each do |key|
-      expect(weather[:hourly][0]).to have_key(key)
+      expect(weather_data[:hourly][0]).to have_key(key)
     end
     keys4.each do |key|
-      expect(weather[:daily][0]).to have_key(key)
+      expect(weather_data[:daily][0]).to have_key(key)
     end
   end
 end
