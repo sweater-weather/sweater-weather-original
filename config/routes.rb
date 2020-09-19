715598121ddfef6fbdf6fcd164f593883873d3ad
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-19 13:54:55 -0600

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/forecast', to: 'forecast#show'
+      get '/backgrounds', to: 'backgrounds#show'
+      post '/sessions', to: 'sessions#create'
+      post '/users', to: 'users#create'
+    end
+  end
 end
