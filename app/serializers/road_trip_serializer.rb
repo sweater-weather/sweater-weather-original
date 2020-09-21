8f445fd6b69b659c0b56e479c689eade5efc9e97
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 13:52:21 -0600

    Create road trip serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -2,5 +2,5 @@ class RoadTripSerializer
   include FastJsonapi::ObjectSerializer
   set_type :road_trip_forecast
   set_id 'nil?'
-  attributes :origin, :destination, :longitude, :latitude, :travel_time, :travel_time_formatted, :arrival_forecast
+  attributes :origin, :destination, :travel_time, :travel_time_formatted, :arrival_forecast
 end
