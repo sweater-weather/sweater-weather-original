999045d85ae565fadb3c16767eb63431e7e6211b
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 09:44:18 -0600

    Create climbing routes poro

---
+++ spec/poros/climbing_routes_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ClimbingRoute do
+  it "can create a ClimbingRoute" do
+    data = {
+      "name": "Dopey Duck",
+      "type": "Trad",
+      "rating": "5.9",
+      "location": [
+        "North Carolina",
+        "2. Northern Mountains Region",
+        "Linville Gorge",
+        "Shortoff Mountain"
+      ],
+      "distance_to_route": "76.876"
+    }
+    cr = ClimbingRoute.new(data)
+    expect(cr.name).to eq(data['name'])
+    expect(cr.type).to eq(data['type'])
+    expect(cr.rating).to eq(data['rating'])
+    expect(cr.location).to eq(data['location'])
+    expect(cr.distance).to eq(data['distance'])
+  end
+end
