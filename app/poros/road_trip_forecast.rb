a621a228f56b027ebe47622fc79b35ab9f644db9
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-20 23:21:33 -0600

    Revert "Create road trip poro"
    This reverts commit 34837f1366369ca184953fbe994481e9eceae667.

--- app/poros/road_trip_forecast.rb
+++
@@ -1,6 +0,0 @@
-class RoadTripForecast
-  def initialize(road_trip_forecast_params)
-    @temperature = road_trip_forecast_params[:temp]
-    @description = road_trip_forecast_params[:weather][0][:description]
-  end
-end
