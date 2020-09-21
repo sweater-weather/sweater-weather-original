3a26b7b88696fb47180db50cbb148447a785e17f
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 14:36:55 -0600

    Create spec to test get request for backgrounds

---
+++ spec/requests/api/v1/background_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Background Endpoint' do
+  it "can make a get request for a background endpoint" do
+    params = {
+      location: 'denver,co'
+    }
+    headers = {
+      'content-type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    get '/api/v1/backgrounds', headers: headers, params: params
+
+    background = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.content_type).to eq('application/json')
+
+    expect(background).to have_key(:data)
+    expect(background[:data]).to have_key(:id)
+    expect(background[:data]).to have_key(:type)
+    expect(background[:data][:type]).to eq('image')
+    expect(background[:data]).to have_key(:attributes)
+    expect(background[:data][:attributes]).to have_key(:location)
+    expect(background[:data][:attributes]).to have_key(:image_url)
+    expect(background[:data][:attributes]).to have_key(:credit)
+    expect(background[:data][:attributes][:credit]).to have_key(:source)
+    expect(background[:data][:attributes][:credit]).to have_key(:author)
+    expect(background[:data][:attributes][:credit]).to have_key(:logo)
+  end
+end
