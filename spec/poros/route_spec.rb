258ed0b9cb888a1da844a0a686c641b39d401c64
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 11:19:27 -0600

    Update test to include set distance method

--- spec/poros/route_spec.rb
+++ spec/poros/route_spec.rb
@@ -12,13 +12,29 @@ RSpec.describe Route do
         "Linville Gorge",
         "Shortoff Mountain"
       ],
-      distance_to_route: "76.876"
     }
     r = Route.new(data)
     expect(r.name).to eq(data[:name])
     expect(r.type).to eq(data[:type])
     expect(r.rating).to eq(data[:rating])
     expect(r.location).to eq(data[:location])
-    expect(r.distance_to_route).to eq(data[:distance_to_route])
+  end
+
+  it "can set distance to route" do
+    data = {
+      name: "Dopey Duck",
+      type: "Trad",
+      rating: "5.9",
+      location: [
+        "North Carolina",
+        "2. Northern Mountains Region",
+        "Linville Gorge",
+        "Shortoff Mountain"
+      ],
+    }
+    r = Route.new(data)
+    expect(r.distance_to_route).to eq(nil)
+    r.set_distance_to_route(5)
+    expect(r.distance_to_route).to eq(5)
   end
 end
