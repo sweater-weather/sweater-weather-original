27e5fddbf1ebff605acd020ff1921346dac03072
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 14:18:21 -0600

    Create road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,14 +1,32 @@
 class RoadTripFacade
-  attr_reader :origin, :destination, :longitude, :latitude, :arrival_forecast, :travel_time, :travel_time_formatted
+  attr_reader :origin, :destination
 
   def initialize(origin, destination)
     @origin = origin
     @destination = destination
     @longitude = coordinates[:lng]
     @latitude = coordinates[:lat]
-    @arrival_forecast = arrival_forecast
-    @travel_time = travel_time
-    @travel_time_formatted = travel_time_formatted
+  end
+
+  def road_trip
+    data = {
+      origin: @origin,
+      destination: @destination,
+      arrival_forecast: arrival_forecast,
+      travel_time: travel_time,
+      travel_time_formatted: travel_time_formatted
+    }
+    RoadTrip.new(data)
+  end
+
+  private
+
+  def weather_service
+    WeatherService.new.get_forecast(@latitude, @longitude)
+  end
+
+  def location_service
+    LocationService.new
   end
 
   def arrival_forecast
@@ -28,13 +46,5 @@ class RoadTripFacade
 
   def coordinates
     c = location_service.data(@destination)[:results][0][:locations][0][:latLng]
-  end
-
-  def weather_service
-    WeatherService.new.get_forecast(@latitude, @longitude)
-  end
-
-  def location_service
-    LocationService.new
   end
 end
