949612c849d3bc0ff9a29401e6ae1bb80af22cb3
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 09:48:25 -0600

    Update spec to use symbols

--- spec/poros/climbing_routes_spec.rb
+++ spec/poros/climbing_routes_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe ClimbingRoute do
   it "can create a ClimbingRoute" do
     data = {
-      "name": "Dopey Duck",
-      "type": "Trad",
-      "rating": "5.9",
-      "location": [
+      name: "Dopey Duck",
+      type: "Trad",
+      rating: "5.9",
+      location: [
         "North Carolina",
         "2. Northern Mountains Region",
         "Linville Gorge",
         "Shortoff Mountain"
       ],
-      "distance_to_route": "76.876"
+      distance_to_route: "76.876"
     }
     cr = ClimbingRoute.new(data)
-    expect(cr.name).to eq(data['name'])
-    expect(cr.type).to eq(data['type'])
-    expect(cr.rating).to eq(data['rating'])
-    expect(cr.location).to eq(data['location'])
-    expect(cr.distance_to_route).to eq(data['distance_to_route'])
+    expect(cr.name).to eq(data[:name])
+    expect(cr.type).to eq(data[:type])
+    expect(cr.rating).to eq(data[:rating])
+    expect(cr.location).to eq(data[:location])
+    expect(cr.distance_to_route).to eq(data[:distance_to_route])
   end
 end
