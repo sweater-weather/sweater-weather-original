79f2645ea7e582afb8d1e0ef10b143e9cc71df83
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 09:48:40 -0600

    Create climbing routes poro

---
+++ app/poros/climbing_route.rb
@@ -0,0 +1,11 @@
+class ClimbingRoute
+  attr_reader :name, :type, :rating, :location, :distance_to_route
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @rating = params[:rating]
+    @location = params[:location]
+    @distance_to_route= params[:distance_to_route]
+  end
+end
