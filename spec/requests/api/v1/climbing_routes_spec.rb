66a1a21d6cc7f0e4b7cae30973dd9c4c324f36c2
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-21 10:16:36 -0600

    Update test to use symbols

--- spec/requests/api/v1/climbing_routes_spec.rb
+++ spec/requests/api/v1/climbing_routes_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Climbing routes' do
-  it "text" do
+  it "can get climbing routes requests" do
     params = {
       'location': 'erwin,tn'
     }
@@ -14,24 +14,23 @@ RSpec.describe 'Climbing routes' do
     get '/api/v1/climbing_routes', headers: headers, params: JSON.generate(params)
 
     climbing_routes = JSON.parse(response.body)
-
     expect(response).to be_successful
     expect(response.content_type).to eq('application/json')
 
-    expect(climbing_routes). to have_key('data')
-    expect(climbing_routes['data']). to have_key('id')
-    expect(climbing_routes['data']). to have_key('type')
-    expect(climbing_routes['data']['type']). to eq('climbing routes')
-    expect(climbing_routes['data']). to have_key('attributes')
-    expect(climbing_routes['data']['attributes']). to have_key('location')
-    expect(climbing_routes['data']['attributes']). to have_key('forecast')
-    expect(climbing_routes['data']['attributes']). to have_key('routes')
-    expect(climbing_routes['data']['attributes']['location']). to have_key('summary')
-    expect(climbing_routes['data']['attributes']['location']). to have_key('temperature')
-    expect(climbing_routes['data']['attributes']['routes'][0]). to have_key('name')
-    expect(climbing_routes['data']['attributes']['routes'][0]). to have_key('type')
-    expect(climbing_routes['data']['attributes']['routes'][0]). to have_key('rating')
-    expect(climbing_routes['data']['attributes']['routes'][0]). to have_key('location')
-    expect(climbing_routes['data']['attributes']['routes'][0]). to have_key('distance_to_route')
+    expect(climbing_routes).to have_key(:data)
+    expect(climbing_routes[:data]).to have_key(:id)
+    expect(climbing_routes[:data]).to have_key(:type)
+    expect(climbing_routes[:data][:type]).to eq(:climbing routes)
+    expect(climbing_routes[:data]).to have_key(:attributes)
+    expect(climbing_routes[:data][:attributes]).to have_key(:location)
+    expect(climbing_routes[:data][:attributes]).to have_key(:forecast)
+    expect(climbing_routes[:data][:attributes]).to have_key(:routes)
+    expect(climbing_routes[:data][:attributes][:location]).to have_key(:summary)
+    expect(climbing_routes[:data][:attributes][:location]).to have_key(temperature)
+    expect(climbing_routes[:data][:attributes][:routes][0]).to have_key(:name)
+    expect(climbing_routes[:data][:attributes][:routes][0]).to have_key(:type)
+    expect(climbing_routes[:data][:attributes][:routes][0]).to have_key(:rating)
+    expect(climbing_routes[:data][:attributes][:routes][0]).to have_key(location)
+    expect(climbing_routes[:data][:attributes][:routes][0]).to have_key(:distance_to_route)
   end
 end
