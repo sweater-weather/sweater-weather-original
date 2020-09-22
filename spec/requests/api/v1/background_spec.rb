23ae1678c93712051c8a98272db9c85ad72f6ee6
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-22 13:43:33 -0600

    Update tests to be more robust and test for what we should not see

--- spec/requests/api/v1/background_spec.rb
+++ spec/requests/api/v1/background_spec.rb
@@ -19,16 +19,26 @@ RSpec.describe 'Background Endpoint', :vcr do
       expect(response.content_type).to eq('application/json')
 
       expect(background).to have_key(:data)
+      expect(background[:data]).to_not be_nil
+      expect(background[:data]).to be_a(Hash)
       expect(background[:data]).to have_key(:id)
+      expect(background[:data][:id]).to eq(nil)
       expect(background[:data]).to have_key(:type)
       expect(background[:data][:type]).to eq('image')
       expect(background[:data]).to have_key(:attributes)
+      expect(background[:data][:attributes].size).to eq(3)
       expect(background[:data][:attributes]).to have_key(:location)
+      expect(background[:data][:attributes][:location]).to_not be_nil
       expect(background[:data][:attributes]).to have_key(:image_url)
+      expect(background[:data][:attributes][:image_url]).to_not be_nil
       expect(background[:data][:attributes]).to have_key(:credit)
+      expect(background[:data][:attributes][:credit]).to be_a(Hash)
       expect(background[:data][:attributes][:credit]).to have_key(:source)
+      expect(background[:data][:attributes][:credit][:source]).to_not be_nil
       expect(background[:data][:attributes][:credit]).to have_key(:author)
+      expect(background[:data][:attributes][:credit][:author]).to_not be_nil
       expect(background[:data][:attributes][:credit]).to have_key(:logo)
+      expect(background[:data][:attributes][:credit][:logo]).to_not be_nil
     end
   end
 
--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -19,33 +19,54 @@ RSpec.describe 'Forecast endpoint', :vcr do
       expect(response.content_type).to eq('application/json')
 
       expect(forecast).to have_key(:data)
+      expect(forecast[:data]).to be_a(Hash)
       expect(forecast[:data]).to have_key(:id)
+      expect(forecast[:data][:id]).to eq(nil)
       expect(forecast[:data]).to have_key(:type)
       expect(forecast[:data][:type]).to eq('forecast')
       expect(forecast[:data]).to have_key(:attributes)
+      expect(forecast[:data][:attributes].size).to eq(4)
       expect(forecast[:data][:attributes]).to have_key(:location)
       expect(forecast[:data][:attributes]).to have_key(:current_forecast)
       expect(forecast[:data][:attributes]).to have_key(:weekly_forecast)
       expect(forecast[:data][:attributes]).to have_key(:hourly_forecast)
 
+      expect(forecast[:data][:attributes][:location]).to be_a(Hash)
+      expect(forecast[:data][:attributes][:current_forecast]).to be_a(Hash)
+      expect(forecast[:data][:attributes][:weekly_forecast]).to be_a(Array)
+      expect(forecast[:data][:attributes][:weekly_forecast].size).to eq(7)
+      expect(forecast[:data][:attributes][:weekly_forecast][0].size).to eq(8)
+      expect(forecast[:data][:attributes][:hourly_forecast].size).to eq(8)
+      expect(forecast[:data][:attributes][:hourly_forecast][0].size).to eq(4)
+      expect(forecast[:data][:attributes][:hourly_forecast]).to be_a(Array)
+
       location_keys = [:city, :state, :country, :country_name]
       location_keys.each do |key|
         expect(forecast[:data][:attributes][:location]).to have_key(key)
+        expect(forecast[:data][:attributes][:location][key]).to_not be_nil
       end
 
       current_forecast_keys = [:description, :current_weather, :icon, :high, :low, :date_time, :date_time_formatted, :feels_like, :humidity, :visibility, :uv_index, :sunrise, :sunrise_time, :sunset, :sunset_time]
       current_forecast_keys.each do |key|
         expect(forecast[:data][:attributes][:current_forecast]).to have_key(key)
+        expect(forecast[:data][:attributes][:current_forecast][key]).to_not be_nil
       end
 
-      weekly_forecast_keys = [:description, :icon, :date_time, :weekday, :high, :low, :snow_percipitation, :rain_percipitation]
+      weekly_forecast_keys = [:description, :icon, :date_time, :weekday, :high, :low]
       weekly_forecast_keys.each do |key|
+        expect(forecast[:data][:attributes][:weekly_forecast][0]).to have_key(key)
+        expect(forecast[:data][:attributes][:weekly_forecast][0][key]).to_not be_nil
+      end
+
+      weekly_forecast_keys_percip =[:snow_percipitation, :rain_percipitation]
+      weekly_forecast_keys_percip.each do |key|
         expect(forecast[:data][:attributes][:weekly_forecast][0]).to have_key(key)
       end
 
       hourly_forecast_keys = [:temperature, :date_time, :date_time_formatted, :icon]
       hourly_forecast_keys.each do |key|
         expect(forecast[:data][:attributes][:hourly_forecast][0]).to have_key(key)
+        expect(forecast[:data][:attributes][:hourly_forecast][0][key]).to_not be_nil
       end
     end
   end
